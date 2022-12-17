8ce800f6171948f17e7f7f4012d22f2708b5639e
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2022-12-17 16:34:03 -0600

    02: Obtener información de los procesos

--- process/01_process.py
+++ process/01_process.py
@@ -5,14 +5,16 @@ import multiprocessing
 
 logging.basicConfig(
     level=logging.DEBUG,
-    format='%(message)s',
+    format='%(process)s - %(processName)s: %(message)s',
 )
 
 def nuevo_proceso():
     logging.info('Hola, soy un nuevo proceso')
-    time.sleep(30)
+    time.sleep(5)
     logging.info('Fin del proceso')
 
 if __name__ == '__main__':
-    process = multiprocessing.Process(target=nuevo_proceso)
+    process = multiprocessing.Process(target=nuevo_proceso, name='Proceso hijo')
     process.start()
+
+    logging.info('Hola desde el proceso padre')
