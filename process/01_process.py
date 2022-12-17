6af72f6aeec3bdb4772baa686d0ebda8d322008c
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2022-12-17 16:38:53 -0600

    03: Argumentos en procesos

--- process/01_process.py
+++ process/01_process.py
@@ -8,13 +8,17 @@ logging.basicConfig(
     format='%(process)s - %(processName)s: %(message)s',
 )
 
-def nuevo_proceso():
+def nuevo_proceso(mensaje):
     logging.info('Hola, soy un nuevo proceso')
     time.sleep(5)
-    logging.info('Fin del proceso')
+    logging.info(mensaje)
 
 if __name__ == '__main__':
-    process = multiprocessing.Process(target=nuevo_proceso, name='Proceso hijo')
-    process.start()
+    # args o kargs
+    process = multiprocessing.Process(
+        target=nuevo_proceso,
+        name='Proceso hijo',
+        args=('Mensaje desde argumento',))
 
+    process.start()
     logging.info('Hola desde el proceso padre')
